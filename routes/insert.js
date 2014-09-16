4705c6e4d03580ea9e5e5ccc5850b203f289fff1
Kobi Luria <kobi@luria.net>
2014-09-16 17:20:30 -0400

    Adding a entity which alredy exists.
    added methods to deal with the problems of a API call to insert a new entity.
    in the case where that entity alredy exists.
    
    [fixed #35]

--- routes/insert.js
+++ routes/insert.js
@@ -5,8 +5,9 @@
 var tools = require('../static/tools');
 var osm_tools = require('../static/osmtools');
 var async = require('async');
+var csv = require('ya-csv');
 
-exports.insertById = function(req, res){
+exports.insertById = function(req, res) {
 
     var osm_id = req.query.osm_id;
     var omuni_id = req.query.omuni_id;
@@ -15,21 +16,47 @@ exports.insertById = function(req, res){
     tools.get_collection('entities', function(err, collection, mongoclient) {
         async.waterfall([
             function(callback) { callback(null, item);},
+            tools.queryDoesntExists,
             osm_tools.build_reverse,
             osm_tools.call_reverse,
             osm_tools.get_osm_results,
             osm_tools.get_correct_osm,
             tools.build_doc
         ], function(err, doc) {
-            //TODO safe insert : i.e will only insert if the object doesn't exists.
+            if(err){
+                res.json({ERROR:err.message});
+                mongoclient.close();
+            }
             //TODO safe insert :  should only insert object where all fields are full.
             //TODO build a standard for the output in cases of insert and update.
             //TODO add a safe insert function in the tools library.
-            collection.insert(doc, function(err, result) {
-                console.log('inserted : ' + result[0].osm_name);
-                res.json({inserted: result[0].osm_name});
-                mongoclient.close();
-            });
+            else {
+                collection.insert(doc, function (err, result) {
+                    console.log('inserted : ' + result[0].osm_name);
+                    res.json({inserted: result[0].osm_name});
+                    mongoclient.close();
+                });
+
+            }
         });
     });
+};
+
+exports.insertByCSV = function(req, res) {
+    console.log('File name is ' + req.files.insertCSV.name);
+    console.log('File size is ' + req.files.insertCSV.size);
+    console.log('File size is ' + req.files.insertCSV.path);
+    var reader = csv.createCsvFileReader(req.files.insertCSV.path, {
+        'separator': ',',
+        'quote': '"',
+        'escape': '"',
+        'comment': ''
+    });
+    reader.addListener('data', function(data) {
+        console.log(data);
+        //TODO check if the entity is updated.
+        //TODO if the entity is not updated should call the insertById
+
+    });
+    res.json({ok:'ok'});
 };
--- static/tools.js
+++ static/tools.js
@@ -7,16 +7,21 @@ var Db = require('mongodb').Db,
     Server = require('mongodb').Server,
     fs = require('fs'),
     async = require('async');
+
+
 /*** @type {string}  Open Muni Api EndPoint **/
 exports.OPEN_MUNI = 'http://ext.openmuni.org.il/v1/entities/';
+/*** @type {string}  NOMINATIM API EndPoint **/
 exports.NOMINATIM = 'http://nominatim.openstreetmap.org/search?';
+/*** @type {string}  NOMINATIM Reverse API EndPoint **/
 exports.NOMINATIM_REVERSE = 'http://nominatim.openstreetmap.org/reverse?';
+/*** @type {string}  The open muni project API EndPoint **/
 exports.OPEN_MUNI = 'http://ext.openmuni.org.il/v1/entities/';
 
 
 
 /*************************************************************************
- * Build a document for the mongo db database.
+ * Build a document for this mongo db database.
  * @param {JSON} osm_entity
  * @param {JSON} api_result
  * @param {Function} callback a callback function, the callback
@@ -39,9 +44,11 @@ exports.build_doc = function(osm_entity, api_result, callback) {
 };
 
 
-
-
-
+/**
+ * Get a connection to the MongoDb collection database
+ * @param {String} name Name of the collection which is trying to be accessed.
+ * @param {Function} callback a callback function
+ */
 exports.get_collection = function(name, callback) {
 
     // Set up the connection to the local db
@@ -53,12 +60,57 @@ exports.get_collection = function(name, callback) {
         var db = mongoclient.db('MoneyMap');
 
         db.collection(name, {} , function(err, collection) {
-            callback(err, collection , mongoclient);
+            callback(err, collection, mongoclient);
 
         });
     });
 }
 
+
+/**
+ * Query if a entity is in the system. returns the latest entity.
+ * @param {String} omuni_id      the id of the entity
+ * @param {async~callback} the callback function to handle
+ */
+exports.queryExists = function(item,callback) {
+    exports.get_collection('entities', function(err, collection, mongoclient) {
+        collection.find({'omuni_id': item.omuni_id},{sort: {date_updated: 1}})
+            .toArray(function(err, results) {
+                callback(null,item[0]); // return the must updated one.
+
+        });
+    });
+};
+
+/**
+ * Query if a entity doesn't exists in the system. returns an Error if it does
+ * @param {String} omuni_id      the id of the entity
+ * @param {async~callback} the callback function to handle
+ */
+exports.queryDoesntExists = function(item,callback) {
+    exports.get_collection('entities', function(err, collection, mongoclient) {
+        collection.find({'omuni_id': item.omuni_id},{sort: {date_updated: 1}})
+            .toArray(function(err, results) {
+                 if(err) {
+                     callback(null,item);
+                 }
+                 if(results.length == 0) {
+                     callback(null,item);
+                 }
+                 else {
+                     callback(new Error('Error - item found'),item);
+                 }
+            });
+    });
+};
+/**
+ * Loop through an api results
+ * each iteration a new object from the results is returend to callback
+ * function
+ * @param {Array} api_results       the API to loop through
+ * @param {Function} result_func    the Function to call with the result
+ * @param {Function} end_func       a function to be called if there was an err
+ */
 exports.loop_api = function(api_results, result_func, end_func) {
     async.whilst(function() {
             return api_results.length ? true : false;
@@ -72,8 +124,15 @@ exports.loop_api = function(api_results, result_func, end_func) {
 
         });
 }
-
-exports.writeJsonp = function(file, obj, wrapper, callback){
+/**
+ * Write a JSON file to a JSONP file
+ * This method will write a JSON Object into a JSONP
+ * @param {String} file the file name for the file
+ * @param {JSON} obj            the JSON object to be wrapped with function
+ * @param {String} wrapper      the function wrapper for the JSON object
+ * @param {Function} callback   the callback function to be called.
+ */
+exports.writeJsonp = function(file, obj, wrapper, callback) {
 
     var str = wrapper;
     str += '(';
@@ -86,7 +145,7 @@ exports.writeJsonp = function(file, obj, wrapper, callback){
         }
         return;
     }
-    str+= ')'
+    str += ')';
     fs.writeFile(file, str, callback);
 }
 
@@ -105,10 +164,25 @@ exports.writeArry2File = function(file, array, var_str, callback){
     str+= ';'
     fs.writeFile(file, str, callback);
 }
-
+/**
+ * A Finally style for closing the Mongo
+ * @param err
+ * @param mongoclient
+ */
 exports.closeMongoClient = function(err, mongoclient){
     if (err) {
         console.log(err);
     }
     mongoclient.close();
+}
+
+/**
+ * This method should throw an error message for a unvaild API request
+ * @function throw Error
+ * @param {Response} res        a express response object
+ * @param {String} message      a message to present to the user
+ */
+//TODO add the error to other logs
+exports.throwError = function (res , msg) {
+    res.json({'ERROR':msg});
 }
